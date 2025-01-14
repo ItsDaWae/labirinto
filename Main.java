e73838d73c2406766ebc461a277af29cd5d04111
ItsDaWae <davide.silvestrini.46@gmail.com>
2025-01-14 09:22:23 +0100

    Main.java
    versione creata da chatGPT con base il mio programma

--- Main.java
+++ Main.java
@@ -1,358 +1,123 @@
+import java.util.*;
 
 public class Main {
-	static void percorso(char[][] pos, int n) {
-		//forse è meglio usare dei metodi per richiamare le funzioni gi su de si, così
-		//non si allunga troppo il codice e non devo vedere i false o i true, ma solo le funzioni
-		//per esempio per fare il controllo di un muro nella direzione selezionata
-		//sarebbe utile poter avere delle info da ricercare e mettere dentro un'altra funzione
-		//senza dover fare il caso particolare per ogniuna delle 4
-		boolean fine=true;
-		boolean inizio=true;
-		
-		int x, y;
-		int xV1=-1;
-		int yV1=-1;
-		int xV2=-1;
-		int yV2=-1;
-		int xV3=-1;
-		int yV3=-1;
-		
-		boolean gi=false;
-		boolean su=false;
-		boolean de=false;
-		boolean si=false;
-		
-		char nodilà='z';
-		char nodilà2='v';
-		char nodilà3='u';
-		
-		
-		//punto d'inizio
-		do {
 
-			x = (int) (Math.random() * (n-0.1));
-			y = (int) (Math.random() * (n-0.1));
-			
-			if(pos[y][x]=='#' && x!=y && (x%2==1 || y%2==1)) {
-			
-				pos[y][x]=' ';
-				System.out.println(pos[y][x]);
-				inizio=false;
-				
-			}
-		}while(inizio==true);
-		
-		if(y==0) {
-			gi=true;
-		}
-		if(y==n-1) {
-			su=true;
-		}
-		if(x==0) {
-			de=true;
-		}
-		if(x==n-1) {
-			si=true;
-		}
-		System.out.println("la x "+x+" e la y -"+y+" in questa posizione");
-		
-		int ciclo=0;
-		int cicloVecchio=0;
-		
-		
-		while(fine) {
-			
-			
-			
-			/*i muri devono essre eretti due cicli dopo che è stato scelto il percorso, nello stesso modo
-			 * probabilmente, così che si possa effettuare il controllo di un altro mmuro nella casella avanti.
-			 * si può tenere il valore di quale direzione usare, e quindi quali muri costruire in una stringa o
-			 * come al solito scegliendo tra 1 2 3 4. e poi fare degli if che attivano la funzione giusta a seconda
-			 * dei tipi di muri che vanno costruiti, lasciando quindi invariati i dintorni, senza aggiungere i "più"*/
-			
-			//creazione muri
-			if(ciclo!=cicloVecchio) {
-			pos = creaMuri(pos, xV3, yV3, nodilà3);
-			}
-			
-			if(gi) {
-				cicloVecchio=ciclo;
-				if(y==n-2) {
-					pos = creaMuri(pos, xV1, yV1, nodilà);
-					y+=1;
-					pos[y][x]='f';
-					System.out.println("la x "+x+" e la y -"+y+" in questa posizione");
-				}else{
-					if(pos[y+1][x]!='#') {
-						y+=1;
-						pos[y][x]=' ';
-						xV3=xV2;
-						xV2=xV1;
-						xV1=x;
-						yV3=yV2;
-						yV2=yV1;
-						yV1=y;
-						gi=false;
-						nodilà3=nodilà2;
-						nodilà2=nodilà;
-						nodilà='a';
-						ciclo++;
-						System.out.println("la x "+x+" e la y -"+y+" in questa posizione");	
-					}else {
-						gi=false;
-					}
-				}
-			}
-			
-			
-			if(su) {
-				cicloVecchio=ciclo;
-				if(y==1) {
-					pos = creaMuri(pos, xV1, yV1, nodilà);
-					y-=1;
-					pos[y][x]='f';
-					System.out.println("la x "+x+" e la y -"+y+" in questa posizione");
-				}else{
-					if(pos[y-1][x]!='#') {
-						y-=1;
-						pos[y][x]=' ';
-						xV3=xV2;
-						xV2=xV1;
-						xV1=x;
-						yV3=yV2;
-						yV2=yV1;
-						yV1=y;
-						su=false;
-						nodilà3=nodilà2;
-						nodilà2=nodilà;
-						nodilà='b';
-						ciclo++;
-						System.out.println("la x "+x+" e la y -"+y+" in questa posizione");
-					}else {
-						su=false;
-					}
-				}	
-			}
-			
-			
-			if(de) {
-				cicloVecchio=ciclo;
-				if(x==n-2) {
-					pos = creaMuri(pos, xV1, yV1, nodilà);
-					x+=1;
-					pos[y][x]='f';
-					System.out.println("la x "+x+" e la y -"+y+" in questa posizione");
-				}else{
-					if(pos[y][x+1]!='#') {
-						x+=1;
-						pos[y][x]=' ';
-						xV3=xV2;
-						xV2=xV1;
-						xV1=x;
-						yV3=yV2;
-						yV2=yV1;
-						yV1=y;
-						de=false;
-						nodilà3=nodilà2;
-						nodilà2=nodilà;
-						nodilà='c';
-						ciclo++;
-						System.out.println("la x "+x+" e la y -"+y+" in questa posizione");
-					}else {
-						de=false;
-					}
-				}
-			}
-			
-			
-			if(si) {
-				cicloVecchio=ciclo;
-				if(x==1) {
-					pos = creaMuri(pos, xV1, yV1, nodilà);
-					x-=1;
-					pos[y][x]='f';
-					System.out.println("la x "+x+" e la y -"+y+" in questa posizione");
-				}else{
-					
-					if(pos[y][x-1]!='#') {
-						x-=1;
-						pos[y][x]=' ';
-						xV3=xV2;
-						xV2=xV1;
-						xV1=x;
-						yV3=yV2;
-						yV2=yV1;
-						yV1=y;
-						si=false;
-						nodilà3=nodilà2;
-						nodilà2=nodilà;
-						nodilà='d';
-						ciclo++;
-						System.out.println("la x "+x+" e la y -"+y+" in questa posizione");
-					}else {
-						si=false;
-					}
-				}
-			}
-			
-			/*
-			 * controlli da fare:
-			 * che non torni indietro§
-			 * che non faccia percorsi larghi due§
-			 * che non rompa muri già costruiti§
-			 * */
-			boolean ok =false;
-			
-			int direzione=0;
-			
-			do {
-				direzione= (int) (Math.random() * 4) +1;
-				
-				if(direzione==1 && nodilà!='b' && x%2==1) {
-					ok=true;
-					gi=true;
-				}
-				if(direzione==2 && nodilà!='a' && x%2==1) {
-					ok=true;
-					su=true;
-				}
-				if(direzione==3 && nodilà!='d' && y%2==1) {
-					ok=true;
-					de=true;
-				}
-				if(direzione==4 && nodilà!='c' && y%2==1) {
-					ok=true;
-					si=true;
-				}
-				
-			}while(ok==false);
-			
-			
-			if(y==0 || y==n-1 || x==0 || x==n-1) {
-				
-				fine=false;
-			}	
-		}
-		
-		aggiornaTabella(pos, n);
-		
-	}
-	static char[][] creaMuri(char[][] pos, int xV3, int yV3, char nodilà3) {
-		
-			if(nodilà3=='a') {
-				if(pos[yV3][xV3+1]!=' ') {
-					pos[yV3][xV3+1]='#';
-				}
-				if(pos[yV3][xV3-1]!=' ') {
-				pos[yV3][xV3-1]='#';
-				}
-				if(pos[yV3+1][xV3]!=' ') {
-					pos[yV3+1][xV3]='#';
-				}
-				if(pos[yV3+1][xV3+1]!=' ') {
-					pos[yV3+1][xV3+1]='#';
-				}
-				if(pos[yV3+1][xV3-1]!=' ') {
-					pos[yV3+1][xV3-1]='#';
-				}
-			}
-			if(nodilà3=='b') {
-				
-				if(pos[yV3][xV3+1]!=' ') {
-					pos[yV3][xV3+1]='#';
-				}
-				if(pos[yV3][xV3-1]!=' ') {
-				pos[yV3][xV3-1]='#';
-				}
-				if(pos[yV3-1][xV3]!=' ') {
-					pos[yV3-1][xV3]='#';
-				}
-				if(pos[yV3-1][xV3+1]!=' ') {
-					pos[yV3-1][xV3+1]='#';
-				}
-				if(pos[yV3-1][xV3-1]!=' ') {
-					pos[yV3-1][xV3-1]='#';
-				}
-			}
-			if(nodilà3=='c') {
-				if(pos[yV3+1][xV3]!=' ') {
-					pos[yV3+1][xV3]='#';
-				}
-				if(pos[yV3-1][xV3]!=' ') {
-					pos[yV3-1][xV3]='#';
-				}
-				if(pos[yV3+1][xV3+1]!=' ') {
-					pos[yV3+1][xV3+1]='#';	
-				}
-				if(pos[yV3-1][xV3+1]!=' ') {
-					pos[yV3-1][xV3+1]='#';
-				}
-				if(pos[yV3][xV3+1]!=' ') {
-					pos[yV3][xV3+1]='#';
-				}
-			}
-			if(nodilà3=='d') {
-				if(pos[yV3+1][xV3]!=' ') {
-					pos[yV3+1][xV3]='#';
-				}
-				if(pos[yV3-1][xV3]!=' ') {
-					pos[yV3-1][xV3]='#';
-				}
-				if(pos[yV3+1][xV3-1]!=' ') {
-					pos[yV3+1][xV3-1]='#';	
-				}
-				if(pos[yV3-1][xV3-1]!=' ') {
-					pos[yV3-1][xV3-1]='#';
-				}
-				if(pos[yV3][xV3-1]!=' ') {
-					pos[yV3][xV3-1]='#';
-				}
-			}
-		
-		return pos;
-	}
+    static class Position {
+        int x, y;
 
-	
-	static void creaTabella(char [][] pos, int n) {
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
-		for(int i=0;i<n;i++) {
-			
-			for(int j=0;j<n;j++) {
-				
-				if(i==0||i==n-1||( j==0 ||j==n-1)) {
-					pos[i][j]='#';
-					
-				}else {
-					pos[i][j]='-';
-					
-				}
-				
-			}
-		}
-	}
-	
-	static void aggiornaTabella(char [][] pos, int n) {
-		for(int i=0;i<n;i++) {
-			
-			for(int j=0;j<n;j++) {
-				
-				System.out.print(pos[i][j]);
-				System.out.print(" ");
-				
-			}
-			System.out.println("");
-		}
-	}
+        Position move(Direction dir) {
+            return new Position(x + dir.dx, y + dir.dy);
+        }
 
-	public static void main(String[] args) {
-		
-		int n=31;
-		char[][] pos = new char[n][n];
-		
-		creaTabella(pos, n);
-		
-		percorso(pos, n);
+        Position wallBetween(Position other) {
+            return new Position((x + other.x) / 2, (y + other.y) / 2);
+        }
+    }
 
-	}
+    enum Direction {
+        UP(0, -2), DOWN(0, 2), LEFT(-2, 0), RIGHT(2, 0);
 
+        final int dx, dy;
+
+        Direction(int dx, int dy) {
+            this.dx = dx;
+            this.dy = dy;
+        }
+    }
+
+    static void percorso(char[][] pos, int n) {
+        Stack<Position> stack = new Stack<>();
+        Random random = new Random();
+
+        // Trova un punto iniziale valido
+        Position start;
+        do {
+            int x = random.nextInt(n);
+            int y = random.nextInt(n);
+            if (x % 2 == 1 && y % 2 == 1 && pos[y][x] == '#') {
+                start = new Position(x, y);
+                break;
+            }
+        } while (true);
+
+        pos[start.y][start.x] = 'S'; // Punto iniziale
+        stack.push(start);
+
+        // Crea un buco nel bordo per il punto di inizio
+        if (start.x == 1) pos[start.y][0] = ' ';
+        else if (start.x == n - 2) pos[start.y][n - 1] = ' ';
+        else if (start.y == 1) pos[0][start.x] = ' ';
+        else if (start.y == n - 2) pos[n - 1][start.x] = ' ';
+
+        Position end = null;
+
+        while (!stack.isEmpty()) {
+            Position current = stack.peek();
+            List<Direction> directions = new ArrayList<>(Arrays.asList(Direction.values()));
+            Collections.shuffle(directions);
+
+            boolean moved = false;
+            for (Direction dir : directions) {
+                Position next = current.move(dir);
+                if (isValidMove(pos, next, n)) {
+                    Position wall = current.wallBetween(next);
+                    pos[wall.y][wall.x] = ' ';
+                    pos[next.y][next.x] = ' ';
+                    stack.push(next);
+                    moved = true;
+                    break;
+                }
+            }
+
+            if (!moved) {
+                end = stack.pop();
+            }
+        }
+
+        if (end != null) {
+            pos[end.y][end.x] = 'E'; // Punto finale
+
+            // Crea un buco nel bordo per il punto di fine
+            if (end.x == 1) pos[end.y][0] = ' ';
+            else if (end.x == n - 2) pos[end.y][n - 1] = ' ';
+            else if (end.y == 1) pos[0][end.x] = ' ';
+            else if (end.y == n - 2) pos[n - 1][end.x] = ' ';
+        }
+
+        aggiornaTabella(pos, n);
+    }
+
+    static boolean isValidMove(char[][] pos, Position p, int n) {
+        return p.x > 0 && p.x < n - 1 && p.y > 0 && p.y < n - 1 && pos[p.y][p.x] == '#';
+    }
+
+    static void creaTabella(char[][] pos, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                pos[i][j] = (i % 2 == 1 && j % 2 == 1) ? '#' : '#';
+            }
+        }
+    }
+
+    static void aggiornaTabella(char[][] pos, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(pos[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 31;
+        char[][] pos = new char[n][n];
+
+        creaTabella(pos, n);
+        percorso(pos, n);
+    }
 }
